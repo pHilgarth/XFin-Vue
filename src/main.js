e5543720dbe35a1fb5e0b8982bf7607b225c1308
Patrick Hilgarth <philgarth@web.de>
2021-02-21 15:21:44 +0100

    implemented router and base route

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
+import AccountView from './components/account-view/AccountView';
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: AccountView },
+    ]
+})
+
+const app = createApp(App)
+
+app.use(router);
+app.mount('#app');
