9c1f63eff1d5574928d496964e4b74ff2060d8f4
Patrick Hilgarth <philgarth@web.de>
2021-03-19 09:33:16 +0100

    added formatDate to numberService

--- src/services/number-service.js
+++ src/services/number-service.js
@@ -6,5 +6,13 @@ export const NumberService = {
           });
     
           return currencyFormat.format(value);
+    },
+
+    formatDate(value) {
+
+        const date = new Date(value);
+
+        //we add a leading zero to day and month and then get the last two chars, only if day or month is below 10 we have leading zeros
+        return `${('0' + date.getDate()).slice(-2)}.${'0' + (date.getMonth() + 1)}.${date.getFullYear()}`
     }
 }
