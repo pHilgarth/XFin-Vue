9f63fd0da8310808ba58ddbf1be2aa2c288a50e1
Patrick Hilgarth <philgarth@web.de>
2021-07-09 17:12:39 +0200

    proceeded work on editaccountholder

--- src/components/edit-account-holder/EditAccountHolder.vue
+++ src/components/edit-account-holder/EditAccountHolder.vue
@@ -11,47 +11,61 @@
         <h1>{{ (Number)($route.params.id) > 0 ? "Kontoinhaber bearbeiten" : "Kontoinhaber hinzufügen" }}</h1>
         <form class="xfin-form">
             <div class="xfin-form__section">
-                <div class="form-group row">
+                <div class="xfin-form__group row">
                     <label class="xfin-form__label col-3" for="name">Name:</label>
-                    <input id="name" :class="{'xfin-form__control col-4': true, 'has-errors': v$.name.$error}" type="text" v-model="v$.name.$model" @blur="v$.name.$touch" @keyup="enforceMaxLength('name', 15)" />
+                    <input id="name" :class="{'xfin-form__control col-4': true, 'has-errors': v$.name.$error}" type="text" :disabled="accountHolderNameDisabled" v-model="name" @blur="v$.name.$touch" @keyup="enforceMaxLength('name', 15)" />
+                    <button class="xfin-form__button btn btn-primary" @click.prevent="toggleAccountHolderNameField" v-if="$route.params.id > 0">{{ accountHolderNameDisabled ? '[edit]' : 'X' }}</button>
+                    <button class="xfin-form__button btn btn-primary" @click.prevent="saveAccountHolderName" v-if="$route.params.id > 0 && !accountHolderNameDisabled">Speichern</button>
                     <p class="xfin-form__error" v-if="v$.name.$error">Bitte gib einen Namen an!</p>
                 </div>
             </div>
-            <div class="xfin-form__section">
-                <p class="xfin-form__notices--accounts">{{accountNotice}}</p>
-                <div class="form-group row">
+            <div class="xfin-form__section" v-if="!this.accountHolder.bankAccounts.length">
+                <div class="xfin-form__group row accounts">
                     <label class="xfin-form__label col-3" for="account">Konto:</label>
-                    <select class="xfin-form__control xfin-form__select col-4" v-model="selectedAccountIndex">
+                    <button class="xfin-form__button btn btn-primary">Konto hinzufügen</button>
+                </div>
+            </div>
+            <div class="xfin-form__section" v-else>
+                <div :class="'xfin-form__notices--accounts alert alert-' + alertType" v-if="accountFormNotice">
+                    <span class="xfin-form__notices__text">{{accountFormNotice}}</span>
+                    <span class="xfin-form__notices__close" @click="closeNotice">&times;</span>
+                </div>
+                <div class="xfin-form__group row accounts">
+                    <label class="xfin-form__label col-3" for="account">Konto:</label>
+                    <select class="xfin-form__control xfin-form__select col-4" v-model="selectedAccountIndex" @change="loadAccountData">
                         <option v-for="(account, index) in accountHolder?.bankAccounts" :key="index" :value="index">{{ account.accountNumber }}</option>
                         <option value="-1" class="new-account">&plus; Neues Konto</option>
                     </select>
                 </div>
-                <div class="form-group row">
+                <div class="xfin-form__group row">
                     <label class="xfin-form__label col-3" for="iban">IBAN:</label>
-                    <input id="iban" :class="{'xfin-form__control col-4': true, 'has-errors': v$.iban.$error}" type="text" v-model="iban" @blur="v$.iban.$touch" />
+                    <input id="iban" :class="{'xfin-form__control col-4': true, 'has-errors': v$.iban.$error}" :disabled="accountFormDisabled" type="text" v-model="iban" @blur="v$.iban.$touch" />
                     <p class="xfin-form__error" v-if="v$.iban.$error">Bitte gib eine gültige IBAN an!</p>
                 </div>
-                <div class="form-group row">
+                <div class="xfin-form__group row">
                     <label class="xfin-form__label col-3" for="bic">BIC:</label>
-                    <input id="bic" :class="{'xfin-form__control col-4': true, 'has-errors': v$.bic.$error}" type="text" v-model="bic" @blur="v$.bic.$touch" />
+                    <input id="bic" :class="{'xfin-form__control col-4': true, 'has-errors': v$.bic.$error}" :disabled="accountFormDisabled" type="text" v-model="bic" @blur="v$.bic.$touch" />
                     <p class="xfin-form__error" v-if="v$.bic.$error">Bitte gib einen gültigen BIC an!</p>
                 </div>
-                <div class="form-group row">
+                <div class="xfin-form__group row">
                     <label class="xfin-form__label col-3" for="bank">Bank:</label>
-                    <input id="bank" :class="{'xfin-form__control col-4': true, 'has-errors': v$.bank.$error}" type="text" v-model="bank" @blur="v$.bank.$touch" @keyup="enforceMaxLength('bank', 30)" />
+                    <input id="bank" :class="{'xfin-form__control col-4': true, 'has-errors': v$.bank.$error}" :disabled="accountFormDisabled" type="text" v-model="bank" @blur="v$.bank.$touch" @keyup="enforceMaxLength('bank', 30)" />
                     <p class="xfin-form__error" v-if="v$.bank.$error">Bitte gib eine Bank an!</p>
                 </div>
-                <div class="form-group row">
+                <div class="xfin-form__group row">
                     <label class="xfin-form__label col-3" for="description">Beschreibung</label>
-                    <input id="description" class="xfin-form__control col-4" type="text" placeholder="(optional)" v-model="description" @keyup="enforceMaxLength('description', 15)" />
+                    <input id="description" class="xfin-form__control col-4" :disabled="accountFormDisabled" type="text" placeholder="(optional)" v-model="description" @keyup="enforceMaxLength('description', 15)" />
                 </div>
-                <div class="form-group row">
+                <div class="xfin-form__group row">
                     <label class="xfin-form__label col-3" for="balance">Kontostand</label>
-                    <input id="balance" :class="{'xfin-form__control col-4': true, 'has-errors': v$.balance.$error}" type="text" v-model="balance" @blur="v$.balance.$touch" />
+                    <input id="balance" :class="{'xfin-form__control col-4': true, 'has-errors': v$.balance.$error}" :disabled="accountFormDisabled" type="text" v-model="balance" @blur="v$.balance.$touch" />
                     <p class="xfin-form__error" v-if="v$.balance.$error">Bitte gib einen gültigen Konstostand an!</p>
                 </div>
-                <button class="xfin-form__button btn btn-primary btn-submit-account" @click.prevent="addAccount" >Konto hinzufügen</button>
+                <button class="xfin-form__button btn btn-primary btn-submit-account" @click.prevent="addAccount" v-if="selectedAccountIndex == -1">Konto hinzufügen</button>
+                <button class="xfin-form__button btn btn-primary btn-edit-account" v-else @click.prevent="toggleEditAccount">{{accountFormDisabled ? 'Konto bearbeiten' : 'Abbrechen'}}</button>
+                <button class="xfin-form__button btn btn-primary btn-update-account" v-if="selectedAccountIndex != -1 && !accountFormDisabled" @click.prevent="updateAccount">Änderungen speichern</button>
             </div>
+            <button class="xfin-form__button btn btn-primary btn-submit-form" @click.prevent="saveAccountHolder">Kontoinhaber speichern</button>
         </form>
     </section>
     </div>
@@ -62,6 +76,7 @@
     import { required } from '@vuelidate/validators'
 
     import { AccountHolderService } from '@/services/account-holder-service';
+    import { NumberService } from '../../services/number-service.js';
 
     import { bicValidator, ibanValidator, balanceValidator } from '@/custom-validators/custom-validators';
 
@@ -72,8 +87,11 @@
         data() {
             return {
                 accountHolder: null,
-                accountNotice: '',
-                selectedAccountIndex: -1,
+                accountHolderNameDisabled: false,
+                accountFormNotice: '',
+                alertType: '',
+                selectedAccountIndex: 0,
+                accountFormDisabled: false,
                 loading: true,
 
                 name: '',
@@ -90,24 +108,46 @@
                 if (this.validateAccountFields()) {
                     this.accountHolder.bankAccounts.push({
                         accountNumber: this.calculateAccountNumber(),
-                        iban: this.iban,
-                        bic: this.bic,
-                        bank: this.bank
+                        iban: this.iban.toUpperCase(),
+                        bic: this.bic.toUpperCase(),
+                        bank: this.bank,
+                        description: this.description,
+                        balance: parseFloat(this.balance.replaceAll('.', '').replace(',', '.'))
                     });
 
                     this.selectedAccountIndex = this.accountHolder.bankAccounts.length - 1;
-                    this.accountNotice = 'Konto erfolgreich angelegt. Um ein weiteres Konto anzulegen, wähle "+ Neues Konto"';
+                    this.accountFormNotice = 'Konto erfolgreich angelegt. Um ein weiteres Konto anzulegen, wähle "+ Neues Konto"';
+                    this.alertType = 'success';
+                    this.accountFormDisabled = true;
+                    this.loadAccountData();
                 }
             },
 
             calculateAccountNumber() {
                 return this.iban.substring(12).replace(/^0+/, '');
+            },
 
+            closeNotice() {
+                this.accountFormNotice = '';
+            },
+
+            toggleAccountHolderNameField() {
+                this.accountHolderNameDisabled = !this.accountHolderNameDisabled;
+
+                if (this.accountHolderNameDisabled) {
+                    this.name = this.accountHolder.name;
+                }
             },
 
             enforceMaxLength(field, length) {
                 if (this[field].length > length) {
                     this[field] = this[field].slice(0, length);
+                }
+            },
+
+            formatCurrency(value) {
+                if (value !== "") {
+                    return NumberService.formatCurrency(value).slice(0, -2);
                 }
             },
 
@@ -118,9 +158,80 @@
                     this.accountHolder = {
                         bankAccounts: []
                     };
+                    this.selectedAccountIndex = -1;
+                }
+                else {
+                    this.accountHolderNameDisabled = true;
+                    this.name = this.accountHolder.name;
+                    this.loadAccountData();
                 }
 
                 this.loading = false;
+            },
+
+            loadAccountData() {
+                if (this.selectedAccountIndex == -1) {
+                    this.iban = '';
+                    this.bic = '';
+                    this.bank = '';
+                    this.description = '';
+                    this.balance = '';
+
+                    this.v$.$reset();
+                    this.accountFormDisabled = false;
+                }
+                else {
+                    this.iban = this.accountHolder.bankAccounts[this.selectedAccountIndex].iban;
+                    this.bic = this.accountHolder.bankAccounts[this.selectedAccountIndex].bic;
+                    this.bank = this.accountHolder.bankAccounts[this.selectedAccountIndex].bank;
+                    this.description = this.accountHolder.bankAccounts[this.selectedAccountIndex].description;
+                    this.balance = this.formatCurrency(this.accountHolder.bankAccounts[this.selectedAccountIndex].balance);
+
+                    this.accountFormDisabled = true;
+                }
+            },
+
+            saveAccountHolder() {
+                this.v$.$touch();
+                
+                if (!this.v$.$errors.length) {
+                    alert('were good to go!');
+                }
+                else {
+                    alert('no, we have errors here');
+                }
+            },
+
+            saveAccountHolderName() {
+                this.v$.name.$touch();
+
+                if (!this.v$.name.$errors.length) {
+                    this.accountHolder.name = this.name;
+                    this.accountHolderNameDisabled = true;
+                }
+            },
+
+            toggleEditAccount() {
+                this.accountFormDisabled = !this.accountFormDisabled;
+
+                if (this.accountFormDisabled) {
+                    this.loadAccountData();
+                }
+            },
+
+            updateAccount() {
+                if (this.validateAccountFields()) {
+                    let bankAccount = this.accountHolder.bankAccounts[this.selectedAccountIndex];
+
+                    bankAccount.accountNumber = this.calculateAccountNumber();
+                    bankAccount.iban = this.iban.toUpperCase();
+                    bankAccount.bic = this.bic.toUpperCase();
+                    bankAccount.bank = this.bank;
+                    bankAccount.description = this.description;
+                    bankAccount.balance = parseFloat(this.balance.replaceAll('.', '').replace(',', '.'));
+
+                    this.loadAccountData();
+                }
             },
 
             validateAccountFields() {
--- src/custom-validators/custom-validators.js
+++ src/custom-validators/custom-validators.js
@@ -22,6 +22,7 @@ export const amountValidator = (value) => value.match(/^(0|[1-9][0-9]*),[0-9]{2}
     allows exavtly 2 decimal places
 
 */
-export const balanceValidator = (value) => value.match(/^-?(0|[1-9][0-9]*),[0-9]{2}$/);
+export const balanceValidator = (value) => value.match(/^-?(0|[1-9](\.?[0-9]{3})*|[1-9][0-9]{1,2}(\.?[0-9]{3})*),[0-9]{2}$/);
 export const bicValidator = (value) => value.match(/^[a-zA-Z]{6}[a-zA-Z0-9]{2}([a-zA-Z0-9]{3})?$/);
 export const ibanValidator = (value) => value.match(/^[a-zA-Z]{2}[0-9]{20}$/);
+//-?(0|[1-9][0-9]*),[0-9]{2}
