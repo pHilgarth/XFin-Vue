fd3ff9cc065f7d90c4ca4fa4a8954096f0c39441
Patrick Hilgarth <philgarth@web.de>
2021-11-09 22:34:24 +0100

    replaced AtomDelete by AtomSpan

---
+++ src/components/atoms/AtomSpan.vue
@@ -0,0 +1,18 @@
+<template>
+    <span :id="id" :class="classList" v-bind="additionalProps" v-html="text"></span>
+</template>
+
+<script>
+export default {
+    props: {
+        id:                 { type: String },
+        classList:          { type: String },
+        text:               {
+                                type: String,
+                                required: true,
+                            },
+
+        additionalProps:    { type: Object },
+    },
+};
+</script>
