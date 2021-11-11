a82ba42b68aaf265b9ad5fb4b4776332be87cb8c
Patrick Hilgarth <ph@webworker.com>
2021-11-11 08:35:19 +0100

    moved editAccountHolder link in the collapsible header with icon

--- src/components/pages/AccountView.vue
+++ src/components/pages/AccountView.vue
@@ -20,12 +20,13 @@
     <MoleculeLoading v-if="loading || !accountHolders" :loadingError="!loading && !accountHolders" />
     
     <section class="account-view-body" v-else>
-      <OrganismCollapsible v-for="accountHolder in accountHolders" :key="accountHolder.id" :config="configureCollapsible(accountHolder)" />
-      
-      <div style="border:1px solid red;margin: 10px 0;padding:10px 0;font-size:16px;">
-        <router-link v-for="accountHolder in accountHolders" :key="accountHolder.id" :to="'/account-holders/' + accountHolder.id">
-          Kontoinhaber bearbeiten
-        </router-link>
+      <div class="account-view__account-holder" v-for="accountHolder in accountHolders" :key="accountHolder.id">
+        <OrganismCollapsible :config="configureCollapsible(accountHolder)" />
+        <span class="account-view__edit" :data-id="accountHolder.id" @click="editAccountHolder">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+          </svg>
+        </span>
       </div>
 
 <!--      <router-link to="/new-account-holder">-->
@@ -129,6 +130,18 @@ export default {
           rows: rows
         },
       };
+    },
+
+    editAccountHolder(event) {
+      let element = event.target;
+
+       while (!element.dataset.id) {
+        element = element.parentNode;
+      }
+
+      const id = element.dataset.id;
+
+       this.$router.push(`/edit-account-holder/${id}`);
     },
 
     async getAccountHolders(includeAccounts = false) {
