adbe9e279a5e9e2b70554ed3b550a0f87fdc85e1
Patrick Hilgarth <ph@webworker.com>
2021-11-12 18:26:44 +0100

    updated AccountSettings.vue

--- src/components/pages/AccountSettings.vue
+++ src/components/pages/AccountSettings.vue
@@ -4,13 +4,14 @@
     <AtomHeadline v-if="!showForm" tag="h1" :text="`Kontoeinstellungen (${account.accountNumber})`" />
     <div class="account-settings__main" v-if="!showForm">
       <AtomParagraph class="account-settings__edit-account-data pb-5" text="Kontodaten bearbeiten" @click="showForm = true" />
-      <MoleculeInputCheckbox classList="pb-3" v-model="accountSettings.effectsExpenses" label="Ausgaben erlauben" :_switch="true"/>
-      <MoleculeInputCheckbox classList="pb-3" v-model="accountSettings.receivesRevenues" label="Einnahmen erlauben" :_switch="true"/>
-      <MoleculeInputCheckbox classList="pb-3" v-model="accountSettings.allowsOverdraft" label="Kontoüberziehung erlauben" :_switch="true"/>
+      <MoleculeInputCheckbox classList="pb-3" v-model="effectsExpenses" label="Ausgaben erlauben" :_switch="true"/>
+      <MoleculeInputCheckbox classList="pb-3" v-model="receivesRevenues" label="Einnahmen erlauben" :_switch="true"/>
+      <MoleculeInputCheckbox classList="pb-3" v-model="allowsOverdraft" label="Kontoüberziehung erlauben" :_switch="true"/>
       <!-- TODO - implement a little 'help'-icon for input fields, which provides info about the field when hovering it (question mark) -->
-      <MoleculeInputText  classList="pb-3" field="balance-threshold" :hasErrors="balanceThresholdErrors" v-model="balanceThreshold"
+      <!-- TODO color red when input is negative -->
+      <MoleculeInputText  classList="pb-5" field="balance-threshold" :hasErrors="balanceThresholdErrors" v-model="balanceThreshold"
                           :validation="v$.balanceThreshold" label="Mindestbetrag" :optional="true" @blur="v$.balanceThreshold.$touch()" />
-      <MoleculeInputText classList="pb-3" field="expenses-threshold" :hasErrors="expensesThresholdErrors" v-model="expensesThreshold" :validation="v$.expensesThreshold"
+      <MoleculeInputText classList="pb-5" field="expenses-threshold" :hasErrors="expensesThresholdErrors" v-model="expensesThreshold" :validation="v$.expensesThreshold"
                          label="Obergrenze für Ausgaben (pro Monat)" :optional="true" @blur="v$.expensesThreshold.$touch()" />
     </div>
     <div class="account-settings__form" v-else>
@@ -28,10 +29,15 @@ import MoleculeInputCheckbox            from '@/components/molecules/MoleculeInp
 import MoleculeInputText                from '@/components/molecules/MoleculeInputText';
 import OrganismAccountForm              from '@/components/organisms/OrganismAccountForm';
 
-import { InternalBankAccountService }   from '@/services/internal-bank-account-service.js';
-import { AccountSettingsService }       from '@/services/account-settings-service.js';
+import { AccountSettingsService }       from '@/services/account-settings-service';
+import { InternalBankAccountService }   from '@/services/internal-bank-account-service';
+import { NumberService }                from '@/services/number-service';
 
-import { accountSettingsValidation }    from '@/validation/validations';
+//import { accountSettingsValidation }    from '@/validation/validations';
+import {
+  balanceThresholdValidator,
+  balanceThresholdMaxValidator,
+  expensesThresholdValidator }          from '@/validation/custom-validators';
 
 export default {
   async created() {
@@ -39,7 +45,7 @@ export default {
 
     result = await this.getAccount();
 
-    if (result.success) {
+    if (result.success) {console.log(this.account);
       result = await this.getAccountSettings(this.account.id);
 
     } else {
@@ -61,12 +67,44 @@ export default {
     OrganismAccountForm,
   },
 
+  computed: {
+    balanceThresholdErrors() { return this.v$.balanceThreshold.$error; },
+    expensesThresholdErrors() { return this.v$.expensesThreshold.$error; },
+  },
+
+  watch: {
+    balanceThreshold() {
+      this.v$.balanceThreshold.$touch();
+
+      const value = this.balanceThreshold === ''
+          ? null
+          : NumberService.amountToString(this.balanceThreshold);
+
+      if (value === null || value < 0) {
+        this.allowsOverdraft = true;
+      }
+      else if (value >= 0) {
+        this.allowsOverdraft = false;
+      }
+    },
+
+    expensesThreshold() {
+      this.v$.expensesThreshold.$touch();
+    }
+  },
+
   data() {
     return {
       dataLoaded: false,
 
       account: null,
-      accountSettings: null,
+
+      effectsExpenses: true,
+      receivesRevenues: true,
+      allowsOverdraft: true,
+      balanceThreshold: null,
+      expensesThreshold: null,
+
       formData: null,
       formHeadline: 'Kontodaten bearbeiten',
       showForm: false,
@@ -102,11 +140,14 @@ export default {
     },
 
     async getAccountSettings(accountId) {
-      alert(accountId);
-      this.accountSettings = await AccountSettingsService.get(accountId);
+      const accountSettings = await AccountSettingsService.get(accountId);
 
-      if (this.accountSettings) {
-        console.log('JO MAN');
+      if (accountSettings) {
+        this.effectsExpenses = accountSettings.effectsExpenses;
+        this.receivesExpenses = accountSettings.receivesExpenses;
+        this.allowsOverdraft = accountSettings.allowsOverdraft;
+        this.balanceThreshold = NumberService.amountToString(accountSettings.balanceThreshold);
+        this.expensesThreshold = NumberService.amountToString(accountSettings.expensesThreshold);
 
         return {
           success: true,
@@ -116,7 +157,7 @@ export default {
       else {
         return {
           success: false,
-          error: 'Error fetching account',
+          error: 'Error fetching accountSettings!',
         };
       }
     }
@@ -128,6 +169,16 @@ export default {
     };
   },
 
-  validations() { return accountSettingsValidation; }
+  validations() {
+    let validation = { expensesThreshold: { expensesThresholdValidator } };
+
+    if (this.account) {
+      validation.balanceThreshold = {
+        amount: balanceThresholdValidator,
+        maxAmount: balanceThresholdMaxValidator(this.account.balance) };
+    }
+
+    return validation;
+  }
 }
 </script>
