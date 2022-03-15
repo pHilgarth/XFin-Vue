e20e35f989d14cdbf88d6e4affd26e6e12c373bb
Patrick Hilgarth <ph@webworker.com>
2022-03-15 18:43:07 +0100

    updated budget manager

--- src/components/molecules/MoleculeBudgetManagerCategory.vue
+++ src/components/molecules/MoleculeBudgetManagerCategory.vue
@@ -1,24 +1,46 @@
 <template>
-  <div class="xfin-card budget-manager__category" :class="{ 'changed' : category.dirty }">
-    <div class="xfin-card__content col-8 budget-manager__category__name">
-      <span>{{ category.name }}</span>
-    </div>
-    <div class="xfin-card__content col-2 budget-manager__category__balance">
-      <!-- TODO - don't use another Atom here - just use AtomInputText and copy the methods from AtomBudgetManagerInput-->
-      <!-- <AtomInputText  :class="`xfin-form__control form-control text-right ${hasErrors ? 'has-errors' : ''}`" :value="balance" :disabled="disabled" -->
-      <!-- <AtomInputText  :class="`xfin-form__control form-control text-right ${hasErrors ? 'has-errors' : ''}`" :value="modelValue" :disabled="disabled" -->
-      <AtomInputText  :class="`xfin-form__control form-control text-right ${hasErrors ? 'has-errors' : ''}`" :value="value" :disabled="disabled"
-                      @input="processInput" />
-      <template v-for="(error, index) in validation?.$errors" :key="index">
-        <AtomParagraph class="xfin-form__error" :text="getErrorMessage(error.$property, error.$validator, errorMessageParams)" />
-      </template>
-    </div>
-      <div v-if="category.dirty" class="budget-manager__category__reset" title="Zurücksetzen" @click="$emit('reset', category)">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
+  <div class="xfin-card budget-manager__category" :class="{ 'changed': category.dirty, 'show-detail': showDetail }">
+    <div class="budget-manager__category-header">
+      <div class="xfin-card__content col-8 budget-manager__category__name">
+        <span>{{ category.name }}</span>
+      </div>
+      <div class="xfin-card__content col-2 budget-manager__category__balance">
+        <!-- TODO - don't use another Atom here - just use AtomInputText and copy the methods from AtomBudgetManagerInput-->
+        <!-- <AtomInputText  :class="`xfin-form__control form-control text-right ${hasErrors ? 'has-errors' : ''}`" :value="balance" :disabled="disabled" -->
+        <!-- <AtomInputText  :class="`xfin-form__control form-control text-right ${hasErrors ? 'has-errors' : ''}`" :value="modelValue" :disabled="disabled" -->
+        <AtomInputText :class="`xfin-form__control form-control text-right ${hasErrors ? 'has-errors' : ''}`"
+                       :value="value" :disabled="disabled"
+                       @input="processInput"/>
+        <template v-for="(error, index) in validation?.$errors" :key="index">
+          <AtomParagraph class="xfin-form__error"
+                         :text="getErrorMessage(error.$property, error.$validator, errorMessageParams)"/>
+        </template>
+      </div>
+      <div v-if="category.dirty" class="budget-manager__category__reset" title="Zurücksetzen"
+           @click="$emit('reset', category)">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+             class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
           <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>
         </svg>
       </div>
+      <div class="budget-manager__detail" @click="showDetail = !showDetail">Detailansicht</div>
+    </div>
+    <hr />
+    <div class="budget-manager__category-detail">
+      <ul>
+        <li>Sample</li>
+        <li>Sample</li>
+        <li>Sample</li>
+        <li>Sample</li>
+        <li>Sample</li>
+        <li>Sample</li>
+        <li>Sample</li>
+        <li>Sample</li>
+        <li>Sample</li>
+        <li>Sample</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -26,9 +48,9 @@
 import AtomInputText from '@/components/atoms/AtomInputText';
 import AtomParagraph from "@/components/atoms/AtomParagraph";
 
-import { NumberService } from '@/services/number-service';
+import {NumberService} from '@/services/number-service';
 
-import { errorMessages } from "@/services/form-error-messages";
+import {errorMessages} from "@/services/form-error-messages";
 
 export default {
   components: {
@@ -37,14 +59,14 @@ export default {
   },
 
   props: {
-    category: { type: Object, required: true },
-    disabled: { type: Boolean },
-    hasErrors: { type: Boolean },
+    category: {type: Object, required: true},
+    disabled: {type: Boolean},
+    hasErrors: {type: Boolean},
     //modelValue: { type: Number },
     //modelValue: { type: String },
-    value: { type: String },
-    validation: { type: Object },
-    errorMessageParams: { type: Object }
+    value: {type: String},
+    validation: {type: Object},
+    errorMessageParams: {type: Object}
   },
 
   // computed: {
@@ -52,6 +74,12 @@ export default {
   //     return NumberService.formatCurrency(this.modelValue, false);
   //   }
   // },
+
+  data() {
+    return {
+      showDetail: false,
+    }
+  },
 
   methods: {
     processInput(event) {
@@ -70,8 +98,8 @@ export default {
         //TODO - delete this whole commented block, when everything is working without it, the validation should already take care of too much decimals
         //if the value has decimal places, valueArray.length is 2
         //if (valueArray.length === 2) {
-          //after the split operation the decimal part is at index 1 - if the decimal part exceeds the length 2, the additional decimal places are cut off  
-          //valueArray[1] = valueArray[1].length > 2 ? valueArray[1].slice(0, 2) : valueArray[1];
+        //after the split operation the decimal part is at index 1 - if the decimal part exceeds the length 2, the additional decimal places are cut off
+        //valueArray[1] = valueArray[1].length > 2 ? valueArray[1].slice(0, 2) : valueArray[1];
         //}
 
         //first we place the thousand separators - we need to reverse it, so we can count up in the loop and set a thousand separator after every third number
@@ -83,9 +111,9 @@ export default {
         for (let i = 0; i < reversedIntPart.length; i++) {
           triplet += reversedIntPart.split('')[i];
           if ((i > 1 && (i + 1) % 3 === 0)) {
-            triplets.push(triplet);triplet='';
-          }
-          else if (i === reversedIntPart.length - 1) {
+            triplets.push(triplet);
+            triplet = '';
+          } else if (i === reversedIntPart.length - 1) {
             triplets.push(triplet);
           }
         }
@@ -103,8 +131,7 @@ export default {
         event.target.selectionEnd = selection < 0 ? 0 : selection;
 
         this.$emit('amount-changed', event.target.value, this.category);
-      }
-      else {
+      } else {
         event.target.value = this.value;
         event.target.selectionStart = caretPosition - 1;
         event.target.selectionEnd = caretPosition - 1;
@@ -140,7 +167,7 @@ export default {
       //         )
       //         ||
       //         event.data !== null && event.data.match(/[0-9]+/) || event.data === ',';
-      
+
       //TODO - move this regex documentation into a doc file
       //these are the teststrings for the long regex above, paste them in regex101.com to see what the reges is doing
       // müssen matchen:
