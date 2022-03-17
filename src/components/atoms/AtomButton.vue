aa7ff9e8238f37c0f333c6428874eef6dadb2947
Patrick Hilgarth <ph@webworker.com>
2022-03-17 20:58:25 +0100

    updated AtomButton

--- src/components/atoms/AtomButton.vue
+++ src/components/atoms/AtomButton.vue
@@ -5,14 +5,8 @@
 <script>
 export default {
     props: {
-        text:               {
-                                type: String,
-                                required: true,
-                            },
-        type:               {
-                              type: String,
-                              required: true,
-                            },
+        text:               { type: String, required: true },
+        type:               { type: String, required: true },
         disabled:           { type: Boolean },
         additionalProps:    { type: Object },
     },
@@ -22,10 +16,14 @@ export default {
         switch (this.type) {
           case 'primary':
             return 'xfin__button--primary';
+          case 'primary-small':
+            return 'xfin__button--primary xfin__button--small';
           case 'light':
             return 'xfin__button--light';
           case 'cancel':
             return 'xfin__button--cancel';
+          case 'cancel-small':
+            return 'xfin__button--cancel xfin__button--small';
         }
       }
   }
