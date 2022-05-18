3967412f18aeeb074776143231f5871acc42daf6
Patrick Hilgarth <ph@webworker.com>
2022-05-18 17:36:23 +0200

    removed change event handler and comment on AtomInputSelect

--- src/components/atoms/AtomInputSelect.vue
+++ src/components/atoms/AtomInputSelect.vue
@@ -1,5 +1,5 @@
 <template>
-  <select class="form-select xfin-form__control" @change="optionChanged">
+  <select class="form-select xfin-form__control">
     <option v-for="(option, index) in options" :key="index" :value="option.value" :disabled="option.disabled">
       {{ option.value }}
     </option>
@@ -10,13 +10,6 @@
 export default {
   props: {
     options: { type: Array, required: true },
-    //value: { type: String, required: true }
   },
-  
-  methods: {
-      optionChanged(event) {
-          this.$emit('option-changed', event.target.value);
-      }
-  }
 }
 </script>
