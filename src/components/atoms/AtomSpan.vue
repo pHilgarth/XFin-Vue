d68c27e000c8cb3561d7d9110c15818812cf1121
Patrick Hilgarth <ph@webworker.com>
2022-05-19 16:36:47 +0200

    removed additionalProps from AtomSpan

--- src/components/atoms/AtomSpan.vue
+++ src/components/atoms/AtomSpan.vue
@@ -1,19 +1,12 @@
 <template>
   <!-- TODO - this component is using the v-html directive - make sure it always gets trusted content! -->
-  <span :id="id" :class="classList" v-bind="additionalProps" v-html="text"></span>
+  <span v-html="text"></span>
 </template>
 
 <script>
 export default {
     props: {
-        id:                 { type: String },
-        classList:          { type: String },
-        text:               {
-                                type: String,
-                                required: true,
-                            },
-
-        additionalProps:    { type: Object },
+        text: { type: String, required: true, },
     },
 };
 </script>
