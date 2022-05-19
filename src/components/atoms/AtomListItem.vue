1af373cc931047786f14c97a8b19d9ea73dac2fd
Patrick Hilgarth <ph@webworker.com>
2022-05-19 16:26:50 +0200

    reformat AtomListItem and added TODO item

--- src/components/atoms/AtomListItem.vue
+++ src/components/atoms/AtomListItem.vue
@@ -1,20 +1,21 @@
 <template>
-    <span class="dev-hint">this component is unsing the v-html directive - make sure it always gets trusted content!</span>
-    <li v-if="config.content" v-html="config.content" v-bind="config.props"></li>
+  <!-- TODO - this component is using the v-html directive - make sure it always gets trusted content! -->
+  <li v-if="config.content" v-html="config.content" v-bind="config.props"></li>
 
-    <li v-else-if="config.component">
-        <component v-if="config.component.content" :is="config.component.tag" v-bind="config.props" v-html="config.component.content" />
-        <component v-else :is="config.component.tag" v-bind="config.props" />        
-    </li>
+  <li v-else-if="config.component">
+    <component v-if="config.component.content" :is="config.component.tag" v-bind="config.props"
+               v-html="config.component.content"/>
+    <component v-else :is="config.component.tag" v-bind="config.props"/>
+  </li>
 </template>
 
 <script>
 export default {
-    props: {
-        config: {
-            type: Object,
-            required: true,
-        }
+  props: {
+    config: {
+      type: Object,
+      required: true,
     }
+  }
 }
 </script>
