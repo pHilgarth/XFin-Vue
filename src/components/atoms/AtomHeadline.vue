21bcf73aea04ad6a658e0fdf2dbe59e81c80d92a
Patrick Hilgarth <ph@webworker.com>
2022-05-28 18:07:18 +0200

    updated AtomHeadline

--- src/components/atoms/AtomHeadline.vue
+++ src/components/atoms/AtomHeadline.vue
@@ -7,8 +7,15 @@
 <script>
 export default {
   props: {
-    tag:    { type: String, required: true },
-    text:   { type: String, required: true },
+    tag: {
+      type: String,
+      required: true,
+      validator: (value) => ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].indexOf(value) !== -1
+    },
+    text: {
+      type: String,
+      required: true
+    },
   },
 };
 </script>
