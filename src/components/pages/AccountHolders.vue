60843cabc0a8c52acfdcfa779a737a558e35adbd
Patrick Hilgarth <p.hilgarth@lights-on.io>
2023-06-22 16:25:36 +0200

    removed link to "NewAccountHolder" from AccountView and placed it in "AccountHolders"

--- src/components/pages/AccountHolders.vue
+++ src/components/pages/AccountHolders.vue
@@ -1,3 +1,15 @@
 <template>
   Kontoinhaber
+
+  <AtomButton type="light" text="Kontoinhaber hinzufügen" @click.prevent="$router.push('/new-account-holder')" />
 </template>
+
+<script>
+  import AtomButton from "@/components/atoms/AtomButton";
+
+  export default {
+    components: {
+      AtomButton,
+    }
+  }
+</script>
